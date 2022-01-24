7d897370b8af7bcf00f7ad59ea71444cd9396a03
godwin <godwin@patricia.com.ng>
2022-01-24 16:16:32 +0100

    first example commit

---
+++ login.php
@@ -0,0 +1,106 @@
+<!-- < ?php
+require('db.php');
+// If form submitted, insert values into the database.
+// if (isset($_REQUEST['username'])){
+        // removes backslashes
+	// $username = "";//stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+	$username = "godwin";//mysqli_real_escape_string($con,$username); 
+	$email = "godwin@gwin.com";//stripslashes($_REQUEST['email']);
+	// $email = "";//mysqli_real_escape_string($con,$email);
+	$password = "gwin";//stripslashes($_REQUEST['password']);
+	// $password = "";//mysqli_real_escape_string($con,$password);
+	$trn_date = date("Y-m-d H:i:s");
+        $query = "INSERT into `users` (username, password, email, trn_date)
+VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+        $result = mysqli_query($con,$query);
+        if($result){
+            echo "<div class='form'>
+<h3>You are registered successfully.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+        }
+//     }
+    else{
+            echo "failed to save data";
+        }
+?>
+< !-- < ? php } ? > --> 
+
+
+<?php
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+      
+      
+      $sql = "SELECT id FROM `users` WHERE username = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+//       $active = $row['fullname'];//active
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+        //  session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+<html>
+   
+   <head>
+      <title>Login Page</title>
+      
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+	
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+					
+            </div>
+				
+         </div>
+			
+      </div>
+
+   </body>
+</html>
